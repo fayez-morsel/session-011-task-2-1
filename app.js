b736214a64861693fcc9a1ce0b2ec2a328611259
Fayez Morsel <fayezmorsel77@gmail.com>
2025-07-25 12:10:54 +0300

    error message

--- app.js
+++ app.js
@@ -41,10 +41,10 @@
 // // Call the functions to that the line below logs out 10
 // console.log(myPoints)
 
-// Try to predict what each of the lines will log out
-console.log("2" + 2) // "22" 
-console.log(11 + 7) //  18
-console.log(6 + "5") // "65"
-console.log("My points: " + 5 + 9) // "My points: 59"
-console.log(2 + 2) // 4
-console.log("11" + "14") //  "1114"
+// // Try to predict what each of the lines will log out
+// console.log("2" + 2) // "22" 
+// console.log(11 + 7) //  18
+// console.log(6 + "5") // "65"
+// console.log("My points: " + 5 + 9) // "My points: 59"
+// console.log(2 + 2) // 4
+// console.log("11" + "14") //  "1114"
