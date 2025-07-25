bcefc3b8d1a3a9c343482ba0006950d4a998dfa4
Fayez Morsel <fayezmorsel77@gmail.com>
2025-07-25 12:03:26 +0300

    string and number

--- app.js
+++ app.js
@@ -19,24 +19,32 @@
 
 // greetLinda()
 
-let myPoints = 3
+// let myPoints = 3
 
-// Create two functions, add3Points() and remove1Point(), and have them
-// add/remove points to/from the myPoints variable
+// // Create two functions, add3Points() and remove1Point(), and have them
+// // add/remove points to/from the myPoints variable
 
-function add3Points() {
-    myPoints += 3
-}
+// function add3Points() {
+//     myPoints += 3
+// }
 
-function remove1Points() {
-    myPoints -= 1
-}
+// function remove1Points() {
+//     myPoints -= 1
+// }
 
-add3Points()
-add3Points()
-add3Points()
-remove1Points()
-remove1Points()
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Points()
+// remove1Points()
 
-// Call the functions to that the line below logs out 10
-console.log(myPoints)
+// // Call the functions to that the line below logs out 10
+// console.log(myPoints)
+
+// Try to predict what each of the lines will log out
+console.log("2" + 2) // "22" 
+console.log(11 + 7) //  18
+console.log(6 + "5") // "65"
+console.log("My points: " + 5 + 9) // "My points: 59"
+console.log(2 + 2) // 4
+console.log("11" + "14") //  "1114"
