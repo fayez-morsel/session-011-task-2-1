4a790088b104b351899433a229b7b49191631fcc
Fayez Morsel <fayezmorsel77@gmail.com>
2025-07-25 15:41:05 +0300

    final result

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// // When the user clicks the purchase button, render out
-// // "Something went wrong, please try again" in the paragraph
-// // that has the id="error".
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
 
-// let errorParagraph = document.getElementById("error");
-// console.log(errorParagraph);
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph);
 
-// function purchase() {
-//   console.log("button clicked");
-//   errorParagraph.textContent = "Something went wrong, please try again";
-// }
+function purchase() {
+  console.log("button clicked");
+  errorParagraph.textContent = "Something went wrong, please try again";
+}
 
 let num1 = 8
 let num2 = 2
