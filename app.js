fb5571e911bd8ab507dc876432c41db500dd207e
Fayez Morsel <fayezmorsel77@gmail.com>
2025-07-25 11:58:17 +0300

    concatinate two varibales in a function

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-// Create two variables, firstName and lastName
-let firstName = "Per"
-let lastName = "Borgen"
-// Concatenate the two variables into a third variable called fullName
+// // Create two variables, firstName and lastName
+// let firstName = "Per"
+// let lastName = "Borgen"
+// // Concatenate the two variables into a third variable called fullName
 
-let fullName = firstName + " " + lastName
+// let fullName = firstName + " " + lastName
 
-// Log fullName to the console
-console.log(fullName)
+// // Log fullName to the console
+// console.log(fullName)
 
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greetLinda() {
+    console.log(greeting + ", " + name + "!")
+}
+
+greetLinda()
 
