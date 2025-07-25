628f5a7e51f05ec459d95595e7a7619276dd0d89
Fayez Morsel <fayezmorsel77@gmail.com>
2025-07-25 12:01:08 +0300

    enc and dec

--- app.js
+++ app.js
@@ -8,14 +8,35 @@
 // // Log fullName to the console
 // console.log(fullName)
 
-let name = "Linda"
-let greeting = "Hi there"
+// let name = "Linda"
+// let greeting = "Hi there"
 
-// Create a function that logs out "Hi there, Linda!" when called
+// // Create a function that logs out "Hi there, Linda!" when called
 
-function greetLinda() {
-    console.log(greeting + ", " + name + "!")
+// function greetLinda() {
+//     console.log(greeting + ", " + name + "!")
+// }
+
+// greetLinda()
+
+let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+
+function add3Points() {
+    myPoints += 3
 }
 
-greetLinda()
+function remove1Points() {
+    myPoints -= 1
+}
 
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
+
+// Call the functions to that the line below logs out 10
+console.log(myPoints)
